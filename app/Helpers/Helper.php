88a653cc8207809e5bcc43d4efd133bf9b314f2f
YahyaKng <yhy.kng@gmail.com>
2018-08-09 12:40:41 +0430

    Helper function manage_web_tags added

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -3,71 +3,72 @@
 namespace App\Helpers;
 
 use Intervention\Image\ImageManagerStatic as Image;
+use App\WebSample;
+use App\WebTag;
 
 class Helper {
 
     /**
-     * @param file $image
+     * Uploads image under public/$folder
      * 
-     * @return boolean
+     * @param file object $image
+     * @param string $folder
+     * @param string $prefix
+     * @param int $width
+     * @param int $height
+     * 
+     * @return string $filename
      */
-    public static function upload_image_old($image, $path, $prefix ="", $storage_path, $width=300, $height=300){
-                
-        //upload_image($request->file('nameOfFormField'), 'prefix', '/directoryInPublicFolder', 300,300);
-
-        // $extension = $path->getClientOriginalExtension();
-        $my_path = public_path('uploads' . $prefix . $image->getFileName() . $image->getClientOriginalExtension());
-        // dd($storage_path);
-        dd($my_path);
-        dd($destination_path = public_path('uploads/websamples/' . $path . $extension));
-        dd($path);
-
-        if (!file_exists($destinationPath)) {
-            mkdir($destinationPath, 0777, true);
-        }
-
-        $filename = $prefix.'_' . date("jFYhis") . '.' . $extention;
-        if(Image::make($path)->fit($width,$height)->save(public_path("websamples/" . $storage_path . $filename))) {
-
-            return $storage_path.$filename;
-        }
-        else {
-            return false;
-        }
-    }
-
-    public static function upload_image($image, $folder, $width, $height){
+    public static function upload_image($image, $folder, $prefix, $width=300, $height=300){
                 
         $imageName = $image->getFilename();
         $imageExtenstion = '.' . $image -> getClientOriginalExtension();
-        $imagePath = public_path($folder . $imageName . $imageExtenstion);
+        
+        $imagePath = public_path($folder);
 
         if (!file_exists($imagePath)) {
             mkdir($imagePath, 0777, true);
         }
+        $filename = $prefix . '_' . date("dmYhis") . $imageExtenstion;
+        
+        $imageFile = $imagePath.$filename;
+        // dd($imageFile);
+        if(Image::make($image->getRealPath())->fit($width,$height)->save($imageFile)) {
 
-        $filename = $prefix.'_' . date("jFYhis") . '.' . $extention;
-        if(Image::make($path)->fit($width,$height)->save(public_path("websamples/" . $storage_path . $filename))) {
-
-            return $storage_path.$filename;
+            return $filename;
         }
         else {
             return false;
         }
     }
 
-    public static function uploadImage($id,$model_name,$image,$file_name) {
+    /**
+     * Manages websamples tags
+     * 
+     * @param integer $smaple_id 
+     * @param array $tags
+     * 
+     * @return 
+     */
+    public static function manage_web_tags($sample_id, $tags=['']) 
+    {
+        $webSample = WebSample::findOrFail($sample_id);
 
-        $destinationPath = public_path('upload/'.$model_name.'/'.$id.'/');
-        if (!file_exists($destinationPath)) {
-            mkdir($destinationPath, 0777, true);
+        // dd($tags);
+
+        if ($tags[0] == '') {
+            $webRes = $webSample->webtags()->detach();
+        } else {
+            $webSample->webtags()->detach();
+            $webRes = $webSample->webtags()->attach($tags);
         }
-        $img = Image::make($image->getRealPath());
-        $img->resize(100, 100, function ($constraint) {
-            $constraint->aspectRatio();
-        })->save($destinationPath . 'thumb_' . $file_name);
-        $upload_res = $image->move($destinationPath, $file_name);
-        return $upload_res;
+        
+        if($webRes) {
+            return 'true';
+        } else {
+            return 'false';
+        }
+        
     }
 
 
